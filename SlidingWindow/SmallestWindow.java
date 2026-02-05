86e5c01b030167c676dfd3e1fc0bf3eabc6c3a78
Rohan <rohantatipamul01@gmail.com>
2026-02-05 13:38:35 +0530

    Program 2 done

---
+++ SlidingWindow/SmallestWindow.java
@@ -0,0 +1,41 @@
+package SlidingWindow;
+
+public class SmallestWindow {
+    public static void main(String args[])
+    {
+        String s="01212";
+    }
+    static int hasAllChars(String s, String pattern)
+    {
+       int res=Integer.MAX_VALUE;
+       boolean zero=false, one=false, two=false;
+       int zeroIndex=-1, oneIndex=-1, twoIndex=-1;
+       for(int i=0;i<s.length();i++)
+       {
+           char ch=s.charAt(i);
+           if(ch=='0')
+            {
+                zero=true;
+                zeroIndex=i;
+
+            }  
+           else if(ch=='1')
+             {
+                one=true;
+                oneIndex=i;
+            }   
+           else if(ch=='2') {
+                two=true;
+                twoIndex=i;
+           }
+
+           if(zero && one && two)
+           {
+               int windowSize = Math.max(zeroIndex, Math.max(oneIndex, twoIndex)) - Math.min(zeroIndex, Math.min(oneIndex, twoIndex));
+               res = Math.min(res, windowSize);
+           }
+       }
+       return res==Integer.MAX_VALUE?-1:res+1;
+       
+    }
+}
